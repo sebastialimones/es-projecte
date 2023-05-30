baf9452257658bc5dd0b5fde121549520b38d363
sebastialimones <sebas.limones@gmail.com>
2023-05-30 16:28:50 +0200

    install react-gtm-module

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { Provider } from 'react-redux';
 import { Routes } from './routes';
 import store from './store';
 
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId:'GTM-NQ553C7'
+};
+
+TagManager.initialize(tagManagerArgs)
+
 export const App = () => (
   <Provider store={ store }>
     <Routes />
