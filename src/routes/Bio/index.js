df0b800f807f84d4ded3f6be0691c0b06089a2c8
sebastialimones <sebas.limones@gmail.com>
2024-08-29 16:39:53 +0200

    text changes

--- src/routes/Bio/index.js
+++ src/routes/Bio/index.js
@@ -40,10 +40,10 @@ export const Bio = () => {
   return (
     <Container>
       <Paragraph>
-        Hola, me llamo Sebastià y acompaño procesos personales. Me formé como terapeuta gestalt en el Aula Balear de Gestalt y también en Psicoterapia Integrativa en el Programa SAT (SAT III) de Claudio Naranjo.
+        Hola, me llamo Sebastià y acompaño procesos personales. Me formé como terapeuta gestalt en el Aula Balear de Gestalt y también en Psicoterapia Integrativa en el Programa SAT (SAT IV) de Claudio Naranjo.
       </Paragraph>
       <Paragraph>
-        Además, tengo formación en constelaciones familiares y un Máster en mediación de conflictos. Me gradué en derecho allá por 2007 y ejercí como abogado durante varios años tanto en Barcelona como en Mallorca.
+        Además, tengo formación en constelaciones familiares y también cursé un Máster en mediación de conflictos. Me gradué en derecho allá por 2007 y ejercí como abogado durante varios años tanto en Barcelona como en Mallorca.
       </Paragraph>
       <Avatar src={IMG_5717} alt="Sebastià"/>
       <Paragraph>
